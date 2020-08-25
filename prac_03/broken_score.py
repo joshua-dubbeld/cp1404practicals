3ce020f35caf70f08d0ce8057e50650187bc73ec
Josh <dubbeldjosh23@gmail.com>
2020-08-25 17:50:05 +1000

    Add random score component to end of main function

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -2,15 +2,33 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
-score = float(input("Enter score: "))
-while score < 0 or score > 100:
-    print("Invalid score")
+
+def main():
     score = float(input("Enter score: "))
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = float(input("Enter score: "))
+    rating = calculate_rating(score)
+    print(rating)
+    random_number = generate_random_number()
+    print("Random number: {}".format(random_number))
+    rating = calculate_rating(random_number)
+    print(rating)
 
-if score > 90:
-    print("Excellent")
-elif score > 50:
-    print("Passable")
-elif score < 50:
-    print("Bad")
+
+def calculate_rating(score):
+    if score > 90:
+        return "Excellent"
+    elif score > 50:
+        return "Passable"
+    elif score < 50:
+        return "Bad"
+
+
+def generate_random_number():
+    number = random.randint(0, 100)
+    return number
+
+main()
