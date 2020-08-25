9bf47c6a27344677de3f5381375acddb3e72e5a3
Josh <dubbeldjosh23@gmail.com>
2020-08-25 18:50:03 +1000

    Change print to file

--- prac_03/scores.py
+++ prac_03/scores.py
@@ -2,11 +2,13 @@ import random
 
 
 def main():
+    out_file = open("results.txt", 'w')
     number_of_scores = int(input("Number of scores: "))
     for i in range(number_of_scores):
         number = generate_random_number()
         result = calculate_rating(number)
-        print("{} is {}".format(number, result))
+        print("{} is {}".format(number, result), file=out_file)
+    out_file.close()
 
 
 def calculate_rating(score):
