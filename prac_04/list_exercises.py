f1fb314153634839d0abc04c970209d8c3b56c21
Josh <dubbeldjosh23@gmail.com>
2020-08-29 19:10:20 +1000

    Add username authorisation

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -2,6 +2,8 @@
 number_of_numbers = int(input("How many numbers? "))
 total = 0
 numbers = []
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+             'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
 for i in range(number_of_numbers):
     number = int(input("Number: "))
     numbers.append(number)
@@ -13,5 +15,12 @@ print("The last number is {}".format(numbers[-1]))
 print("The smallest number is {}".format(min(numbers)))
 print("The largest number is {}".format(max(numbers)))
 print("The average of the numbers is {:.2f}".format(average))
+
+name = input("Username: ")
+if name in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
+
 
 
