c5be95d263766c90f78be70bb09ecfd3e3c84825
Josh <dubbeldjosh23@gmail.com>
2020-08-29 21:44:38 +1000

    Add quick pick duplication checking

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -6,14 +6,21 @@ MAX_VALUE = 45
 QUICKPICKS_PER_LINE = 6
 
 quick_pick_line = []
+used_quickpick_numbers = []
 
 number_of_lines = int(input("How many quick picks? "))
 for line in range(number_of_lines):
     for number in range(QUICKPICKS_PER_LINE):
         quickpick = random.randint(MIN_VALUE, MAX_VALUE)
+        while quickpick in used_quickpick_numbers:
+            quickpick = random.randint(MIN_VALUE, MAX_VALUE)
+
         quick_pick_line.append(quickpick)
+        used_quickpick_numbers.append(quickpick)
+
     quick_pick_line.sort()
     for number in range(len(quick_pick_line)):
         print("{:3}".format(quick_pick_line[number]), end="")
     print()
     quick_pick_line.clear()
+    used_quickpick_numbers.clear()
