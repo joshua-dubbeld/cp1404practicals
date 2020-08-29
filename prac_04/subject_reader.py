b7a8f6f8d0ed7e0190919615ae5c89abd0d2be4f
Josh <dubbeldjosh23@gmail.com>
2020-08-29 18:28:39 +1000

    Append data to list and format it into a string

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,22 +8,32 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    display_subject_details(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    data = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        data.append(parts)
+    return data
     input_file.close()
+
+
+def display_subject_details(data):
+    """Get data in list form and produce details in a nicely formatted string"""
+    number_of_subjects = len(data)
+    for subject in range(number_of_subjects):
+        subject_code = data[subject][0]
+        subject_teacher = data[subject][1]
+        subject_number_of_students = data[subject][2]
+        print("{} is taught by {:12} and has {:3} students".format(subject_code, subject_teacher,
+                                                                   subject_number_of_students))
+
 
 
 main()
