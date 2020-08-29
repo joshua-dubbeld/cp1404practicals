ca288346ec4ab295ce482cab17f181d0be1e9704
Josh <dubbeldjosh23@gmail.com>
2020-08-29 19:24:06 +1000

    Add numbers above 9 list comprehension

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -38,5 +38,7 @@ almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
 numbers = [int(number) for number in almost_numbers]
 print(numbers)
 
-# TODO: use a list comprehension to create a list of only the numbers that are
+# list comprehension to create a list of only the numbers that are
 # greater than 9 from the numbers (not strings) you just created
+numbers_above_9 = [number for number in numbers if number > 9]
+print(numbers_above_9)
