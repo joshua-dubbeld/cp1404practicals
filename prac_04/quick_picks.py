d0e92f8e65ba044718dae1d325b235b96ef193f4
Josh <dubbeldjosh23@gmail.com>
2020-08-29 21:49:19 +1000

    Add comments for better understanding

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -12,6 +12,8 @@ number_of_lines = int(input("How many quick picks? "))
 for line in range(number_of_lines):
     for number in range(QUICKPICKS_PER_LINE):
         quickpick = random.randint(MIN_VALUE, MAX_VALUE)
+
+        # Checks if the generated quick pick has already been used. If it has, it generates a new one.
         while quickpick in used_quickpick_numbers:
             quickpick = random.randint(MIN_VALUE, MAX_VALUE)
 
@@ -22,5 +24,7 @@ for line in range(number_of_lines):
     for number in range(len(quick_pick_line)):
         print("{:3}".format(quick_pick_line[number]), end="")
     print()
+
+    # Clears lists for next line
     quick_pick_line.clear()
     used_quickpick_numbers.clear()
