5e78b411cd09fd0ba6bb7de9e8a444322014b243
Josh <dubbeldjosh23@gmail.com>
2020-09-08 16:35:21 +1000

    Add for loop to check how many occurrences of the words in the string there are

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -8,3 +8,12 @@ words_dict = {}
 words = input("Text: ")
 word_list = words.split()
 print(word_list)
+
+for word in word_list:
+    if word in words_dict:
+        words_dict[word] += 1
+    else:
+        words_dict[word] = 1
+
+for word in words_dict:
+    print("{} : {}".format(word, words_dict[word]))
