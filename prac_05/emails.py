f7ce7b13800ca452f408c14323cce9d2a1545340
Josh <dubbeldjosh23@gmail.com>
2020-09-08 17:20:09 +1000

    Add email input and name and adds to emails_dict

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -2,3 +2,21 @@
 CP1404 Practical
 Store names from email input in dictionary
 """
+
+emails_dict = {}
+
+email = input("Email: ")
+while email != "":
+    email_alias = email.split('@')
+    name_list = email_alias[0].split('.')
+    name = ' '.join([word for word in name_list]).title()
+    choice = input("Is your name {}? (Y/n) ".format(name))
+    if choice.lower() == 'y':
+        emails_dict[email] = name
+    else:
+        name = input("Name: ").title()
+        emails_dict[email] = name
+
+    email = input("Email: ")
+
+print(emails_dict)
