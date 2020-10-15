af1062b5fe94df2332d974562b5344a5c7ea1ab4
Josh <dubbeldjosh23@gmail.com>
2020-10-15 23:37:15 +1000

    Initial commit - Intermediate Exercises

---
+++ prac_08/silver_service_taxi_test.py
@@ -0,0 +1,6 @@
+from prac_08.silver_service_taxi import SilverServiceTaxi
+
+fancy_taxi = SilverServiceTaxi("Fancy Taxi", 100, 2)
+fancy_taxi.drive(18)
+print(fancy_taxi)
+print(fancy_taxi.get_fare())
