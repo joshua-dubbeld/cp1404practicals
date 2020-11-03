0cd302833ef5e43c04a448e416faea857be62aa5
Josh <dubbeldjosh23@gmail.com>
2020-11-04 00:34:06 +1000

    Complete moving files into directories

--- prac_09/sort_files_2.py
+++ prac_09/sort_files_2.py
@@ -1,19 +1,34 @@
 import os
-
-extensions = []
-extension_categories = {}
+import shutil
 
 os.chdir("FilesToSort")
 
-for filename in os.listdir('.'):
-    extension_index = filename.find('.')
-    extension = filename[extension_index + 1:]
-    if extension not in extensions:
-        extensions.append(extension)
-        category = input("What category would you like to sort {} files into? ".format(extension))
-        extension_categories[extension] = category
-    try:
-        os.mkdir(extension)
-    except FileExistsError:
-        pass
-print(extension_categories)
+
+def main():
+    extensions = []
+    extension_categories = {}
+
+    for filename in os.listdir('.'):
+        if os.path.isdir(filename):
+            continue
+        extension_index = filename.find('.')
+        extension = filename[extension_index + 1:]
+        if extension not in extensions:
+            extensions.append(extension)
+            category = input("What category would you like to sort {} files into? ".format(extension)).title()
+            extension_categories[extension] = category
+        try:
+            os.mkdir(extension_categories[extension])
+        except FileExistsError:
+            pass
+        shutil.move(filename, extension_categories[extension])
+
+
+def remove_directories():
+    for directory in os.listdir('.'):
+        if os.path.isdir(directory):
+            os.rmdir(directory)
+
+
+# remove_directories()
+main()
