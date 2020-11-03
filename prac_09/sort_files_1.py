2655d926d22f8b6a67f37597d09823dff68ef5f1
Josh <dubbeldjosh23@gmail.com>
2020-11-04 00:10:11 +1000

    Create folders and add extensions to a list

--- prac_09/sort_files_1.py
+++ prac_09/sort_files_1.py
@@ -1,8 +1,16 @@
 import os
 
+extensions = []
+
 os.chdir("FilesToSort")
 
 for filename in os.listdir('.'):
     extension_index = filename.find('.')
     extension = filename[extension_index + 1:]
-    print(extension)
+    if extension not in extensions:
+        extensions.append(extension)
+    try:
+        os.mkdir(extension)
+    except FileExistsError:
+        print("'{}' directory already exists.".format(extension))
+print(extensions)
