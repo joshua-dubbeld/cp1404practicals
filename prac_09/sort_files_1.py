01f0253e33ebb2fcbccb74ad2980f7ff6b1c6ad7
Josh <dubbeldjosh23@gmail.com>
2020-11-04 00:00:40 +1000

    Get extension from filename

--- prac_09/sort_files_1.py
+++ prac_09/sort_files_1.py
@@ -1,3 +1,8 @@
 import os
 
 os.chdir("FilesToSort")
+
+for filename in os.listdir('.'):
+    extension_index = filename.find('.')
+    extension = filename[extension_index + 1:]
+    print(extension)
