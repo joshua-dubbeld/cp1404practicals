c1ce99d5ba4af965285fbcf2757505a706f42846
Josh <dubbeldjosh23@gmail.com>
2020-11-03 21:19:43 +1000

    Complete file rewrites

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -2,7 +2,6 @@
 CP1404/CP5632 Practical
 Demos of various os module examples
 """
-import shutil
 import os
 
 
@@ -10,32 +9,41 @@ def main():
     """Demo os module functions."""
     print("Starting directory is: {}".format(os.getcwd()))
 
-    os.chdir('Lyrics')
-    for directory_name, subdirectories, filenames in os.walk('.'):
-        print("Directory:", directory_name)
-        print("\tcontains subdirectories:", subdirectories)
-        print("\tand files:", filenames)
+    # Change to desired directory
+    os.chdir('Lyrics/Christmas')
 
-        print("(Current working directory is: {})".format(os.getcwd()))
+    # Loop through each file in the (current) directory
+    for filename in os.listdir('.'):
+        # Ignore directories, just process files
+        if os.path.isdir(filename):
+            continue
 
-        for filename in os.listdir(directory_name):
-            # Ignore directories, just process files
-            if os.path.isdir(filename):
-                continue
-
-            new_name = get_fixed_filename(filename)
-            if filename == new_name:
-                continue
-            print("Renaming {} to {}".format(filename, new_name))
-            os.rename(os.path.join(directory_name, filename), new_name)
-
-    # Print a list of all files in current directory
-    print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
+        filename.replace(" ", "_").replace(".TXT", ".txt")
+        new_name = get_fixed_filename(filename)
+        # if filename == new_name:
+        #     continue
+        print("Renaming {} to {}".format(filename, new_name))
+        os.rename(filename, new_name)
 
 
 def get_fixed_filename(filename):
     """Return a 'fixed' version of filename."""
-    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    new_name = ''
+    for i, character in enumerate(filename):
+        new_character = character
+        try:
+            if filename[i] == ' ':
+                new_character = '_'
+            elif filename[i - 1] == '_':
+                new_character = character.upper()
+            elif character == '_':
+                new_character = character
+            elif filename[i + 1].isupper():
+                new_character = character + '_'
+        except IndexError:
+            pass
+        new_name += new_character
+
     return new_name
 
 
